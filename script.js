7e26ee1eee2ad75851a303efbbf27c0b03a396cd
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-23 10:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function getWindowSize() {
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  var windowSizeElement = document.getElementById('sizeInfo').querySelector('h1');
+  windowSizeElement.textContent = 'Width: ' + windowWidth + ' and Height: ' + windowHeight;
+}
+
+// Attach the event listener to the "resize" event
+window.addEventListener('resize', getWindowSize);
+
+// Initial call to get the window size
+getWindowSize();
